3ebdf705558781494f1e312c2c0120a863260d4a
anhkhoakz <113995598+anhkhoakz@users.noreply.github.com>
2024-08-04 12:17:23 +0700

    Update options for view

--- index.js
+++ index.js
@@ -1,7 +1,74 @@
+const mostUsedCategories = [
+    {
+        name: "Login",
+        fontawesome: "fa-solid fa-passport",
+    },
+    { name: "Education", fontawesome: "fa-solid fa-school" },
+    {
+        name: "Learning Management System",
+        fontawesome: "fa-solid fa-bars-progress",
+    },
+    { name: "Student Affairs", fontawesome: "fa-solid fa-user-graduate" },
+    { name: "Inspire Library", fontawesome: "fa-solid fa-book-open-reader" },
+];
+
+const mostUsedCategoryItems = {
+    Login: [
+        {
+            name: "Student Portal",
+            link: "https://old-stdportal.tdtu.edu.vn/",
+            fontawesome: "fa-solid fa-right-to-bracket",
+        },
+    ],
+    Education: [
+        {
+            name: "Program Specification - Cumulative Plots",
+            link: "https://learninginfo.tdtu.edu.vn/sv_xemctdt",
+            fontawesome: "fa-solid fa-share-nodes",
+        },
+        {
+            name: "Course Schedule",
+            link: "https://lichhoc-lichthi.tdtu.edu.vn/tkb2.aspx",
+            fontawesome: "fa-solid fa-calendar-days",
+        },
+        {
+            name: "Learning Outcomes",
+            link: "https://ketquahoctap.tdtu.edu.vn/",
+            fontawesome: "fa-solid fa-star",
+        },
+    ],
+    "Learning Management System": [
+        {
+            name: "E-learning",
+            link: "https://old-stdportal.tdtu.edu.vn/main/elearningv2",
+            fontawesome: "fa-solid fa-laptop-file",
+        },
+        {
+            name: "E-learning IT",
+            link: "https://elit.tdtu.edu.vn/",
+            fontawesome: "fa-brands fa-gitlab",
+        },
+    ],
+    "Student Affairs": [
+        {
+            name: "Results of Training Evaluation",
+            link: "https://old-stdportal.tdtu.edu.vn/main/hoatdongphongtrao",
+            fontawesome: "fa-solid fa-trophy",
+        },
+    ],
+    "Inspire Library": [
+        {
+            name: "Reserve a Room",
+            link: "http://reservearoom-sv.tdtu.edu.vn/",
+            fontawesome: "fa-solid fa-building",
+        },
+    ],
+};
+
 const categories = [
     {
         name: "Login",
-        fontawesome: " fa-solid fa-passport",
+        fontawesome: "fa-solid fa-passport",
     },
     { name: "Education", fontawesome: "fa-solid fa-school" },
     {
@@ -37,7 +104,6 @@ const categoryItems = {
             link: "https://lichhoc-lichthi.tdtu.edu.vn/xemlichthi.aspx",
             fontawesome: "fa-solid fa-magnet",
         },
-
         {
             name: "Learning Outcomes",
             link: "https://ketquahoctap.tdtu.edu.vn/",
@@ -97,7 +163,7 @@ const categoryItems = {
     ],
     "Inspire Library": [
         {
-            name: "Reservea Room",
+            name: "Reserve a Room",
             link: "http://reservearoom-sv.tdtu.edu.vn/",
             fontawesome: "fa-solid fa-building",
         },
@@ -110,23 +176,44 @@ const categoryItems = {
 };
 
 const categoriesHTML = document.querySelector("#categories");
+const selectElement = document.querySelector("select");
 
-categories.forEach((category) => {
-    const categoryHeader = document.createElement("h2");
+function renderCategories(selectedOption) {
+    categoriesHTML.innerHTML = ""; // Clear previous content
+    const data =
+        selectedOption === "essential-feature"
+            ? mostUsedCategories
+            : categories;
+    const items =
+        selectedOption === "essential-feature"
+            ? mostUsedCategoryItems
+            : categoryItems;
 
-    categoryHeader.innerHTML = `<i class="fas ${category.fontawesome}"></i> <span>${category.name}</span>`;
-    categoriesHTML.appendChild(categoryHeader);
+    data.forEach((category) => {
+        const categoryHeader = document.createElement("h2");
+        categoryHeader.innerHTML = `<i class="fas ${category.fontawesome}"></i> <span>${category.name}</span>`;
+        categoriesHTML.appendChild(categoryHeader);
 
-    const categoryItemsHTML = categoryItems[category.name].map((item) => {
-        const categoryHTML = document.createElement("p");
-        const link = document.createElement("a");
-        link.href = item.link;
-        link.innerHTML = `<i class="fas ${item.fontawesome}"></i> <span>${item.name}</span>`;
-        link.target = "_blank";
-        categoryHTML.appendChild(link);
-        categoryHTML.id = item.name.replace(/\s+/g, "_");
-        return categoryHTML;
+        const categoryItemsHTML = items[category.name].map((item) => {
+            const categoryHTML = document.createElement("p");
+            const link = document.createElement("a");
+            link.href = item.link;
+            link.innerHTML = `<i class="fas ${item.fontawesome}"></i> <span>${item.name}</span>`;
+            link.target = "_blank";
+            categoryHTML.appendChild(link);
+            categoryHTML.id = item.name.replace(/\s+/g, "_");
+            return categoryHTML;
+        });
+
+        categoriesHTML.append(...categoryItemsHTML);
     });
+}
 
-    categoriesHTML.append(...categoryItemsHTML);
+// Initial render with essential features
+renderCategories("essential-feature");
+
+// Event listener for dropdown changes
+selectElement.addEventListener("change", (event) => {
+    const selectedOption = event.target.value;
+    renderCategories(selectedOption);
 });
